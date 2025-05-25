5568020c06a2866d87feb501f607541d7f2723f9
Gwen <gwendolyn.whelan@gmail.com>
2025-05-25 16:11:06 -0400

    use device identifier in s3 object name

--- lib/nerves_hub/logs/batch_processor.ex
+++ lib/nerves_hub/logs/batch_processor.ex
@@ -10,7 +10,7 @@ defmodule NervesHub.Logs.BatchProcessor do
   def start_link(_), do: Task.start_link(fn ->
     Process.sleep(@interval)
     Logger.info("[#{__MODULE__}] sending logs...")
-    from(l in Log, order_by: l.logged_at, limit: @batch_size)
+    from(l in Log, order_by: l.logged_at, limit: @batch_size, preload: [:device])
     |> Repo.all()
     |> store_logs()
     |> Task.await_many()
@@ -31,7 +31,7 @@ defmodule NervesHub.Logs.BatchProcessor do
     Enum.map(logs, fn l ->
       req = ExAws.S3.put_object(
         @bucket,
-        "#{l.device_id}/#{l.logged_at}",
+        "#{l.device.identifier}/#{l.logged_at}",
         "[#{l.level}] #{l.message}",
         []
       )
